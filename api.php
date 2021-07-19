0eb4f9dce35dfa6055ef3be02c296fe73a5f94f4
Roderic Page <rdmpage@gmail.com>
2021-07-19 17:29:27 +0100

    Create an API (and use it on home page)

---
+++ api.php
@@ -0,0 +1,170 @@
+<?php
+
+// api
+
+error_reporting(E_ALL);
+
+require_once('vendor/autoload.php');
+
+use Seboettg\CiteProc\StyleSheet;
+use Seboettg\CiteProc\CiteProc;
+
+//----------------------------------------------------------------------------------------
+function default_display()
+{
+	echo "hi";
+}
+
+//----------------------------------------------------------------------------------------
+function display_parse($citations, $format= 'json', $callback = '')
+{
+
+	$config['crf_path'] = '/usr/local/bin';
+	$config['crf_path'] = dirname(__FILE__) . '/src';
+
+	// clean up to improve parsing
+	
+	// remove bad characters
+	
+	$citations = preg_replace('/\x18/', "", $citations);
+	
+	// space between colon and following alphanumeic token token
+	$citations = preg_replace('/:([A-Za-z0-9])/', ": $1", $citations);
+	
+	// trim letters form dates
+	$citations = preg_replace('/([0-9]{4})[a-z]/', "$1", $citations);
+
+	// 
+	//print_r($citations);
+	
+	$tmpdir = dirname(__FILE__) . '/tmp';
+	$timestamp = time();
+	
+	$base_name =  $tmpdir . '/' . $timestamp;
+	
+	$text_filename = $base_name . '.txt';
+	
+	file_put_contents($text_filename, trim($citations));	
+	
+	$command = 'php refs_to_train.php ' . $text_filename;	
+	//echo '<pre>' . $command . '</pre><br>';	
+	system($command);
+	
+	$command = 'php parse_train.php ' . $base_name . '.src.xml';
+	//echo '<pre>' . $command . '</pre><br>';
+	system($command);
+	
+	$command = $config['crf_path'] . '/crf_test  -m core.model ' . $base_name . '.src.train > ' . $base_name . '.out.train';
+	//echo '<pre>' . $command . '</pre><br>';
+	system($command);
+	
+	$command = 'php parse_results_to_xml.php ' . $base_name . '.out.train > ' . $base_name . '.out.xml';
+	//echo '<pre>' . $command . '</pre><br>';
+	system($command);
+	
+	$command = 'php parse_results_to_native.php ' . $base_name . '.out.xml';
+	//echo '<pre>' . $command . '</pre><br>';
+
+	
+	
+	$output = array();	
+	exec($command, $output);	
+	$json = join("\n", $output);
+	
+	$response = '';
+	$response_mimetype = "text/plain";
+		
+	switch ($format)
+	{
+		case 'html':
+			$csl = json_decode($json);
+			$style = 'apa';
+		
+			$style_sheet = StyleSheet::loadStyleSheet($style);
+			$citeProc = new CiteProc($style_sheet);
+			$response = $citeProc->render($csl, "bibliography");
+			break;		
+	
+	
+		case 'json':
+			default:			
+			$response = $json;
+			$response_mimetype = "application/json";
+			break;
+	
+	}
+	
+	header("Content-type: " . $response_mimetype);
+	
+	if ($callback != '')
+	{
+		echo $callback . '(';
+	}
+	echo $response;
+	if ($callback != '')
+	{
+		echo ')';
+	}			
+	
+	
+	
+
+
+}
+
+
+
+//----------------------------------------------------------------------------------------
+function main()
+{
+	$callback = '';
+	$handled = false;	
+	
+	// If no query parameters 
+	if (count($_GET) == 0)
+	{
+		default_display();
+		exit(0);
+	}
+	
+	if (isset($_GET['callback']))
+	{	
+		$callback = $_GET['callback'];
+	}
+	
+	// Submit job
+	if (!$handled)
+	{
+		$citations = (isset($_GET['text']) ? $_GET['text'] : '');	
+		if ($citations)
+		{
+			$format = 'json';
+			
+			if (isset($_GET['format']))
+			{
+				$format = $_GET['format'];
+			}			
+			
+			if (!$handled)
+			{
+				display_parse($citations, $format, $callback);
+				$handled = true;
+			}
+			
+		}
+	}
+	
+	
+	if (!$handled)
+	{
+		default_display();
+	}	
+
+}
+
+
+main();
+
+
+?>
+
--- index.php
+++ index.php
@@ -1,86 +1,98 @@
 <?php
-
-$config['crf_path'] = '/usr/local/bin';
-
-$config['crf_path'] = dirname(__FILE__) . '/src';
-
-$citations = (isset($_GET['citations']) ? $_GET['citations'] : '');
-
-if ($citations) 
-{
-	// clean up to improve parsing
-	
-	// space between colon and following alphanumeic token token
-	$citations = preg_replace('/:([A-Za-z0-9])/', ": $1", $citations);
-	
-	// trim letters form dates
-	$citations = preg_replace('/([0-9]{4})[a-z]/', "$1", $citations);
-
-	// 
-	//print_r($citations);
-	
-	$tmpdir = dirname(__FILE__) . '/tmp';
-	$timestamp = time();
-	
-	$base_name =  $tmpdir . '/' . $timestamp;
-	
-	$text_filename = $base_name . '.txt';
-	
-	file_put_contents($text_filename, trim($citations));	
-	
-	$command = 'php refs_to_train.php ' . $text_filename;	
-	//echo '<pre>' . $command . '</pre><br>';	
-	system($command);
-	
-	$command = 'php parse_train.php ' . $base_name . '.src.xml';
-	//echo '<pre>' . $command . '</pre><br>';
-	system($command);
-	
-	$command = $config['crf_path'] . '/crf_test  -m core.model ' . $base_name . '.src.train > ' . $base_name . '.out.train';
-	//echo '<pre>' . $command . '</pre><br>';
-	system($command);
-	
-	$command = 'php parse_results_to_xml.php ' . $base_name . '.out.train > ' . $base_name . '.out.xml';
-	//echo '<pre>' . $command . '</pre><br>';
-	system($command);
-	
-	$command = 'php parse_results_to_native.php ' . $base_name . '.out.xml';
-	//echo '<pre>' . $command . '</pre><br>';
-	system($command);
-	
-}
-else
-{
-
 ?>
 
 <html>
 <head>
 	<meta charset="utf-8" />
 	<title>Citations</title>
+	<style>
+		body {
+			font-family:sans-serif;
+			padding:20px;
+		}
+		
+	button {
+		font-size:1em;
+	}
+			
+	</style>
+	<script src="js/jquery.js" type="text/javascript"></script>			
+	
 </head>
 <body>
 <h1>
 	Parse citations
 </h1>
 
-<div>
 
+
+<!--
+<div>
 	<form id="form" action="./" method="get">
-<textarea id="citations"  name="citations" rows="5" cols="80">
+<textarea style="font-size:1em;" id="citations"  name="text" rows="20" cols="60" >
 Bokermann, W. C. A. 1950. Rescriçao e novo nome genérico para Coelonotus fissilis Mir. Rib.,1920. Papéis Avulsos de Zoologia. São Paulo 9: 215–222. 
 </textarea>
 
     <br />
-   <input type="submit" value="Parse" />
+   <input style="border:1px solid black;-webkit-appearance: none;appearance: none;font-size:1em;" type="submit" value="Parse" />
    </form>
 </div>
 
+-->
 
+
+<h2>Text to parse</h2>
+<p>Enter citation strings, one per line.</p>
+
+<div>
+	<textarea style="font-size:1em;box-sizing: border-box;width:100%;" id="text"  name="text" rows="10" >
+Möllendorff O (1894) On a collection of land-shells from the Samui Islands, Gulf of Siam. Proceedings of the Zoological Society of London, 1894: 146–156.
+de Morgan J (1885) Mollusques terrestres & fluviatiles du royaume de Pérak et des pays voisins (Presqúile Malaise). Bulletin de la Société Zoologique de France, 10: 353–249.
+Morlet L (1889) Catalogue des coquilles recueillies, par M. Pavie dans le Cambodge et le Royaume de Siam, et description ďespèces nouvelles (1). Journal de Conchyliologie, 37: 121–199. 
+Naggs F (1997) William Benson and the early study of land snails in British India and Ceylon. Archives of Natural History, 24:37–88.
+	</textarea>
+    <br />
+   <button onclick="parse()">Parse</button>
+</div>
+
+<h3>API</h3>
+
+<p><a id="api" href=".">API call</a></p>
+
+<h2>Output</h2>
+<div id="output" style="font-family:monospace;white-space:pre-wrap;background-color:rgb(50,50,50);color:#8EFA00;"></div>
+
+<!--
+<h2>OpenURL</h2>
+<div id="openurl"></div>
+-->
+
+
+
+<script>
+function parse() {
+
+	document.getElementById("output").innerHTML = "";
+
+	var text = document.getElementById("text").value;
+	
+	var url = 'api.php?text=' + encodeURIComponent(text);
+	
+	document.getElementById("api").setAttribute('href', url);
+	
+	 $.getJSON(url + '&callback=?', function(data) {
+		if (data) {
+			document.getElementById("output").innerHTML = JSON.stringify(data, null, 2);
+		}
+	 });
+}
+
+
+</script>
 
 </body>
 </html>
 
 <?php
-}
+
 ?>
